a859f5a5bc743ef153821496772cfb2d5dee0e8a
Caio Neves <caio.nevesleite@gmail.com>
2018-01-17 22:50:24 +0000

    Route for new albums

--- server.js
+++ server.js
@@ -55,6 +55,10 @@ app.post("/login", function(req, res){
   });
 });
 
+app.post("/new/album", function(req, res){
+  console.log(req.body);
+});
+
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log("server listening at", process.env.IP + ":" + process.env.PORT);
 });
